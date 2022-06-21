31f3edb247d08bd46084f8220b276e66f112a26c
g. nicholas d'andrea <nick@gnidan.org>
2022-06-21 16:50:47 -0400

    Add `--no-attribution` and `--no-source` flags
    ... to skip printing the attribution header and source-listing footer,
    respectively

--- packages/abi-to-sol/src/defaults.ts
+++ packages/abi-to-sol/src/defaults.ts
@@ -2,3 +2,5 @@ export const name = "MyInterface";
 export const license = "UNLICENSED";
 export const solidityVersion = ">=0.7.0 <0.9.0";
 export const prettifyOutput = true;
+export const outputAttribution = true;
+export const outputSource = true;
